3cd876cbd2a5e9a4595341982dbeb4341d567e2f
Ivan.Orlov <ivan@morrr.com>
2016-11-10 14:48:18 +0200

    Version 2

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 module.exports = {
 	hello: "fuse-box-test-rogue-files!",
-	foo: require("./foo.js")
+	foo: require("./foo.js"),
+	fancyNewMethod: function() {
+		console.log("Something fancy that is comming with version 2.0.0");
+	}
+
 }
